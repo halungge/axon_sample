294a0b72e7e97fa516efc204adb4a87c8c654862
Magdalena Luz <magdalena.luz@teqable.com>
2020-11-09 14:49:18 +0100

    add initially generated liquibase file and generattion command

--- src/main/java/ch/mlz/axon/todo/app/tenancy/TenantConnectionProvider.java
+++ src/main/java/ch/mlz/axon/todo/app/tenancy/TenantConnectionProvider.java
@@ -2,6 +2,7 @@ package ch.mlz.axon.todo.app.tenancy;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.HibernateException;
 import org.hibernate.engine.jdbc.connections.spi.MultiTenantConnectionProvider;
 import org.springframework.stereotype.Component;
 
@@ -14,7 +15,7 @@ import java.sql.SQLException;
 @RequiredArgsConstructor
 public class TenantConnectionProvider implements MultiTenantConnectionProvider {
 
-    public final static String DEFAULT_SCHEMA = "nottiswil";
+    public final static String DEFAULT_SCHEMA = "public";
     final private DataSource datasource;
 
 
@@ -33,19 +34,29 @@ public class TenantConnectionProvider implements MultiTenantConnectionProvider {
     public Connection getConnection(String tenantIdentifier) throws SQLException {
         log.info("Get connection for tenant {}", tenantIdentifier);
         final Connection connection = getAnyConnection();
-        connection.setSchema(tenantIdentifier);
+        try {
+            connection.setSchema(tenantIdentifier);
+        }catch(SQLException exception){
+            throw new HibernateException("could not alter JDBC connection to set schema to '"+ tenantIdentifier+"'");
+        }
         return connection;
     }
 
     @Override
     public void releaseConnection(String tenantIdentifier, Connection connection) throws SQLException {
         log.info("Release connection for tenant {}", tenantIdentifier);
-        connection.setSchema(DEFAULT_SCHEMA);
-        releaseAnyConnection(connection);
+        try {
+            connection.setSchema(DEFAULT_SCHEMA);
+        }catch(SQLException exception){
+            throw new HibernateException("could not alter JDBC connection to set schema to '"+ DEFAULT_SCHEMA+"'");
+        }finally {
+           connection.close();
+        }
     }
 
     @Override
     public boolean supportsAggressiveRelease() {
+        // TODO or false??
         return false;
     }
 
